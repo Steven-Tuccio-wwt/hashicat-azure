5ce8732c8e4babc85dac921101f939b959ef0472
Steve Tuccio <steven.tuccio@wwt.com>
2021-03-18 22:22:25 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,6 +2,6 @@
 module "network" {
   source  = "app.terraform.io/StevenTuccio-Training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "other-resource-group"
+  resource_group_name = "steven-tuccio-workshop"
 }
 
